6d3d49fa53da0b9fd5ffa83e881d9d7222a6f844
JulianaYS <u20201b782@upc.edu.pe>
2023-10-17 08:27:05 -0500

    feat(enrollments): added progress record business logic, and enrollment relationship optimization.

--- src/main/java/com/acme/learning/platform/learning/domain/model/entities/ProgressRecordItem.java
+++ src/main/java/com/acme/learning/platform/learning/domain/model/entities/ProgressRecordItem.java
@@ -1,10 +1,11 @@
 package com.acme.learning.platform.learning.domain.model.entities;
 
+import com.acme.learning.platform.learning.domain.model.aggregates.Enrollment;
 import com.acme.learning.platform.learning.domain.model.valueobjects.ProgressStatus;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 
+import java.time.LocalDate;
 import java.util.Date;
 
 @Entity
@@ -12,13 +13,18 @@ public class ProgressRecordItem {
     @Id
     private Long id;
     @Getter
-    private Long enrollmentId;
+    @ManyToOne
+    @JoinColumn(name="enrollment_id")
+    private Enrollment enrollmentId;
     @Getter
-    private Long tutorialId;
+    @ManyToOne
+    @JoinColumn(name = "tutorial_id")
+    private Tutorial tutorialId;
     private ProgressStatus status;
+    private Date startedAt;
     private Date completedAt;
 
-    public ProgressRecordItem(Long enrollmentId, Long tutorialId){
+    public ProgressRecordItem(Enrollment enrollmentId, Tutorial tutorialId){
         this.enrollmentId = enrollmentId;
         this.tutorialId = tutorialId;
         this.status = ProgressStatus.NOT_STARTED;
@@ -27,8 +33,34 @@ public class ProgressRecordItem {
     public ProgressRecordItem(){
 
     }
+    public void start(){
+        this.status=ProgressStatus.STARTED;
+        this.startedAt=new Date();
+    }
     public void complete(){
         this.status = ProgressStatus.COMPLETED;
         this.completedAt = new Date();
+    }
+    public boolean isCompleted(){
+        return this.status==ProgressStatus.COMPLETED;
+    }
+    public boolean isInProgress(){
+        return this.status==ProgressStatus.STARTED;
+    }
+    public boolean isNotStarted(){
+        return this.status==ProgressStatus.NOT_STARTED;
+    }
+    public long calculateDaysElapsed(){
+        if(isNotStarted()) return 0;
+        var defaultTimeZone=java.time.ZoneId.systemDefault();
+
+        var fromDate=this.startedAt.toInstant();
+
+        var toDate=this.completedAt==null ?
+                LocalDate.now().atStartOfDay(defaultTimeZone).toInstant() :
+                this.completedAt.toInstant();
+                return java.time.Duration.between(fromDate,toDate).toDays();
+
+
     }
 }
